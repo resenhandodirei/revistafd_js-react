46a8d27bc55e5657da5581490bb8a8a8c3033f38
resenhandodirei <larimscorrea@gmail.com>
2025-01-04 17:13:54 -0300

    feat: border do header da revista foi retirado

--- revistafd/src/components/Header/HeaderStyles.tsx
+++ revistafd/src/components/Header/HeaderStyles.tsx
@@ -7,7 +7,7 @@ export const styles = StyleSheet.create({
     alignItems: 'center',
     padding: 16,
     backgroundColor: '#FFF0DA', // Off white para o fundo
-    borderBottomWidth: 1,
+    //borderBottomWidth: 1,
     borderBottomColor: '#FFA500', // Laranja para destacar a separação
     textTransform: 'uppercase',
   },
@@ -25,7 +25,7 @@ export const styles = StyleSheet.create({
     marginHorizontal: 8,
   },
   navText: {
-    fontSize: 16,
+    fontSize: 14,
     color: '#120A8F', // Azul marinho para itens de navegação
   },
   dropdown: {
@@ -36,7 +36,7 @@ export const styles = StyleSheet.create({
     padding: 12,
     borderRadius: 4,
     shadowColor: '#000',
-    shadowOpacity: 0.1,
+    shadowOpacity: 0.2,
     shadowRadius: 4,
     shadowOffset: { width: 0, height: 2 },
     elevation: 3,
